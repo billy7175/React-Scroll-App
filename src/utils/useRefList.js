43e2676e21b44365329f65b6c73e7ac6f9d11c08
unknown <whgus439@gmail.com>
2021-07-04 23:15:29 +0900

    Re-Create utils

---
+++ src/utils/useRefList.js
@@ -0,0 +1,34 @@
+import {useRef, useCallback} from 'react';
+
+
+export default function useRefList(setPageNumber, hasMore) {
+  const observer = useRef();
+
+  const lastPostElement = useCallback(
+    (ele) => {
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          //reached bottom
+          setPageNumber((prev) => prev + 1);
+        }
+      });
+      if (ele) observer.current.observe(ele);
+    },
+    [hasMore, setPageNumber]
+  );
+
+  // focus
+  const inputWrapperObserver = useRef();
+  const inputObserver = useRef();
+  function inputFocus() {
+    inputObserver.current.focus();
+    inputWrapperObserver.current.style.borderColor = "rgba(59, 130, 246, 1)"; 
+  }
+
+  function removeInputFocus(){
+    inputWrapperObserver.current.style.borderColor = "#777"; 
+  }
+
+  return { lastPostElement,inputWrapperObserver,inputObserver, inputFocus, removeInputFocus  }
+}
